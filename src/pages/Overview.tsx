f624b42569b5ad5693f0a01eae159a6ca73b6ac9
Reza Hadi Fairuztama <rreza510@gmail.com>
2023-08-17 03:37:52 +0700

    Add "react-calendar@^4.6.0" on Overview.tsx

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { ReceiptDatabase } from '../data/database';
 import { Receipt } from '../dataclass';
+import Calendar from 'react-calendar';
+import { Value } from 'react-calendar/dist/cjs/shared/types';
 
 interface Props {
   database: ReceiptDatabase;
 }
 
 function Overview({database} : Props) {
+  
   const [receipts, setReceipts] = useState<Receipt[]>([]);
   const [isLoading, setLoading] = useState(true);
+  const [selectedDate, setSelectedDate] = useState<Date|undefined>(new Date())
 
   useEffect(() => {
     database.GetReceipts().then(v => {
@@ -49,12 +53,26 @@ function Overview({database} : Props) {
       ).totalToString()
       : "none!";
   }
+  function onCalendarChangeDate(value: Value) {
+    if (value) {
+      if (Array.isArray(value)) setSelectedDate(value[0] || value[1] || undefined)
+      else setSelectedDate(value)
+    }
+    
+  } 
   if (isLoading) return <>Loading...</>
   return ( 
     <div>
       <p></p>
       <p>This month&apos;s saving: {getSaving()}</p>
       <p>This month&apos;s spending: {getSpending()}</p>
+      <hr/>
+      <p>
+        <Calendar
+          onChange={(v) => onCalendarChangeDate(v)}
+          value={selectedDate}
+        />
+      </p>
     </div>
   );
 }
