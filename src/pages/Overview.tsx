f13aa2d5b213ec0acbc512f11884b3b27d7b8345
Reza Hadi Fairuztama <rreza510@gmail.com>
2023-08-18 21:08:50 +0700

    remove calendar usage on Overview.tsx

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { ReceiptDatabase } from '../data/database';
 import { Receipt } from '../dataclass';
-import Calendar from 'react-calendar';
-import { Value } from 'react-calendar/dist/cjs/shared/types';
 
 interface Props {
   database: ReceiptDatabase;
@@ -12,18 +10,15 @@ function Overview({database} : Props) {
   
   const [receipts, setReceipts] = useState<Receipt[]>([]);
   const [isLoading, setLoading] = useState(true);
-  const [selectedDate, setSelectedDate] = useState<Date|undefined>(new Date())
 
   useEffect(() => {
     database.GetReceipts().then(v => {
-      if (v.length > 0) {
-        const currDate = new Date()
-        const thisMonthReceipts = v.filter(val => 
-          val.date.getMonth() == currDate.getMonth() 
-          && val.date.getFullYear() == currDate.getFullYear()
-          )
-        setReceipts(thisMonthReceipts);
-      }
+      const currDate = new Date()
+      const thisMonthReceipts = v.filter(val => 
+        val.date.getMonth() == currDate.getMonth() 
+        && val.date.getFullYear() == currDate.getFullYear()
+        )
+      setReceipts(thisMonthReceipts);
       setLoading(false)
     })
   }, [])
@@ -53,13 +48,6 @@ function Overview({database} : Props) {
       ).totalToString()
       : "none!";
   }
-  function onCalendarChangeDate(value: Value) {
-    if (value) {
-      if (Array.isArray(value)) setSelectedDate(value[0] || value[1] || undefined)
-      else setSelectedDate(value)
-    }
-    
-  } 
   if (isLoading) return <>Loading...</>
   return ( 
     <div>
@@ -67,12 +55,6 @@ function Overview({database} : Props) {
       <p>This month&apos;s saving: {getSaving()}</p>
       <p>This month&apos;s spending: {getSpending()}</p>
       <hr/>
-      <p>
-        <Calendar
-          onChange={(v) => onCalendarChangeDate(v)}
-          value={selectedDate}
-        />
-      </p>
     </div>
   );
 }
