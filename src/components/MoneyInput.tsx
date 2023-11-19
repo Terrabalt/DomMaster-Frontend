b4eec63004206893440590f29076723f1e62f0fa
Reza Hadi Fairuztama <rreza510@gmail.com>
2023-11-19 15:11:34 +0700

    fixes MoneyInput cursor reset when input invalid

--- src/components/MoneyInput.tsx
+++ src/components/MoneyInput.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import { Money } from '../dataclass';
 import { CurrencyDescription, CurrencySymbol, ListCurrencyCodes } from '../currencyCode';
 import InputWithValidator from './InputWithValidator';
@@ -12,6 +12,9 @@ interface Props {
 }
 
 export default function MoneyInput({value, onChange, validators}: Props) {
+  const [prevSelectionStart, setPrevSelectionStart] = useState<number|null>(null)
+  const [prevSelectionEnd, setPrevSelectionEnd] = useState<number|null>(null)
+  const [resetSelection, setResetSelection] = useState(false)
   const inputText = useMemo(() => {
     const whole = value.amount / BigInt(100)
     const decimal = value.amount % BigInt(100)
@@ -24,16 +27,19 @@ export default function MoneyInput({value, onChange, validators}: Props) {
       const decimalMarkRemoved = (e.target.value as string).replace(DecimalMark, "")
       const bigInt = BigInt(decimalMarkRemoved)
       onChange(new Money(bigInt, value.currency))
-    } catch (e) {
-      if (e instanceof SyntaxError) return
-      else throw e
+    } catch (err) {
+      if (err instanceof SyntaxError) {
+        setResetSelection(true)
+        return
+      }
+      else throw err
     }
   }
 
   return (
     <>
       <select
-        style={{maxWidth:100}}
+        style={{maxWidth:45}}
         name="newItemCurrency"
         value={value.currency}
         onChange={e => onChange(new Money(value.amount, e.target.value))}
@@ -45,6 +51,17 @@ export default function MoneyInput({value, onChange, validators}: Props) {
         }
       </select>
       <InputWithValidator
+        onSelect={(e: React.ChangeEvent<HTMLInputElement>) => {
+          if (resetSelection) {
+            e.target.setSelectionRange(prevSelectionStart, prevSelectionEnd)
+            setResetSelection(false)
+          }
+          
+          if (prevSelectionStart != e.target.selectionStart || prevSelectionEnd != e.target.selectionEnd) {
+            setPrevSelectionStart(e.target.selectionStart)
+            setPrevSelectionEnd(e.target.selectionEnd)
+          }
+        }}
         aria-label={CurrencySymbol(value.currency)}
         name="newItemCost"
         value={inputText}
